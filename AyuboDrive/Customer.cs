a3bd2bb59046f7c5e806cea76718c8a0ed11518c
Shalin Kulawardane <nirvan.work@gmail.com>
2022-08-06 02:56:42 +0530

    Completed adding functionality to Customer.cs

--- AyuboDrive/Customer.cs
+++ AyuboDrive/Customer.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace AyuboDrive
 {
@@ -10,27 +12,90 @@ namespace AyuboDrive
     class Customer
     {
         public int CustomerID { get; }
-        private String FirstName { get; }
-        private String LastName { get; }
+        private string FirstName { get; }
+        private string LastName { get; }
         private string ContactNumber { get; }
-        //private databasehandler databasehandler;
+        private const string connectionString = @"Data Source=DESKTOP-0CECDCR;Initial Catalog=AyuboDriveV1;Integrated Security=True";
 
         public Customer(int customerID, string firstName, string lastName, string contactNumber)
         {
-            this.CustomerID = customerID;
-            this.FirstName = firstName;
-            this.LastName = lastName;
-            this.ContactNumber = contactNumber;
+            CustomerID = customerID;
+            FirstName = firstName;
+            LastName = lastName;
+            ContactNumber = contactNumber;
         }
 
-        public static void registerCustomer(string firstName, string lastName, string contactNumber)
+        public static bool RegisterCustomer(string firstName, string lastName, string contactNumber)
         {
-            //Add the customer details into the database
+            string query = "INSERT INTO Customer VALUES(@customerID, @firstName, @lastName, @contactNumber)";
+
+            try
+            {
+                using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+                {
+                    sqlConnection.Open();
+                    SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+                    sqlCommand.Parameters.AddWithValue("@customerID", 8);
+                    sqlCommand.Parameters.AddWithValue("@firstName", firstName);
+                    sqlCommand.Parameters.AddWithValue("@lastName", lastName);
+                    sqlCommand.Parameters.AddWithValue("@contactNumber", contactNumber);
+                    sqlCommand.ExecuteNonQuery();
+                    return true;
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"{e}", "An error occurred when updating", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            return false;
         }
 
-        public void updateCustomer() {}
+        public static bool UpdateCustomer(int customerID, string firstName, string lastName, string contactNumber)
+        {
+            string query = "UPDATE Customer SET firstName = @firstName, lastName = @lastName, contactNumber = " +
+                "@contactNumber WHERE customerID = @customerID";
+            
+            try
+            {
+                using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+                {
+                    sqlConnection.Open();
+                    SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+                    sqlCommand.Parameters.AddWithValue("@customerID", customerID);
+                    sqlCommand.Parameters.AddWithValue("@firstName", firstName);
+                    sqlCommand.Parameters.AddWithValue("@lastName", lastName);
+                    sqlCommand.Parameters.AddWithValue("@contactNumber", contactNumber);
+                    sqlCommand.ExecuteNonQuery();
+                    return true;
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"{e}", "An error occurred when updating", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            return false;
+        }
 
-        public void deleteCustomer() {}
+        public static bool DeleteCustomer(int customerID)
+        {
+            string query = "DELETE FROM Customer WHERE customerID = @customerID";
 
+            try
+            {
+                using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+                {
+                    sqlConnection.Open();
+                    SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+                    sqlCommand.Parameters.AddWithValue("@customerID", customerID);
+                    sqlCommand.ExecuteNonQuery();
+                    return true;
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"{e}", "An error occurred when deleting", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            return false;
+        }
     }
 }
