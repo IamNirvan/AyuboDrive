efea2c10e54e3cc4d33c76107f28e50a4eedfe50
Shalin Kulawardane <nirvan.work@gmail.com>
2022-08-11 03:23:42 +0530

    Modified PackageType.cs

--- AyuboDrive/PackageType.cs
+++ AyuboDrive/PackageType.cs
@@ -10,16 +10,16 @@ namespace AyuboDrive
 {
     class PackageType
     {
-        public int PackageID { get; }
-        private string PackageName;
-        private int MaxKm;
-        private int MaxHour;
-        private double ExtraKmRate;
-        private double ExtraHourRate;
-        private double StandardRate;
-        private const string connectionString = @"Data Source=DESKTOP-0CECDCR;Initial Catalog=AyuboDriveV1;Integrated Security=True";
+        public string PackageID { get; set; }
+        private string PackageName { get; set; }
+        public int MaxKm { get; set; }
+        public int MaxHour { get; set; }
+        public double ExtraKmRate { get; set; }
+        public double ExtraHourRate { get; set; }
+        private double StandardRate { get; set; }
+        private static QueryHandler queryHandler = new QueryHandler();
 
-        public PackageType(int packageID, string packageName, int maxKm, int maxHour, double extraKmRate, double extraHourRate, double standardRate)
+        public PackageType(string packageID, string packageName, int maxKm, int maxHour, double extraKmRate, double extraHourRate, double standardRate)
         {
             PackageID = packageID;
             PackageName = packageName;
@@ -30,83 +30,50 @@ namespace AyuboDrive
             StandardRate = standardRate;
         }
 
-        public bool RegisterPackageType()
+        public static PackageType RegisterPackageType(string packageName, int maxKm, int maxHour, double extraKmRate, double extraHourRate, double standardRate)
         {
+            string packageID = "";
             string query = "INSERT INTO PackageType VALUES (@packageID, @packageName, @maxKm, @maxHour, @extraKmRate, @extraHourRate, @standardRate)";
+            string[] parameters = { "@packageID", "@packageName", "@maxKm", "@maxHour", "@extraKmRate", " @extraHourRate", "@standardRate" };
+            object[] values = { packageID, packageName, maxKm, maxHour, extraKmRate, extraHourRate, standardRate };
 
-            try
+            if (queryHandler.HandleInsertDeleteUpdateQuery(query, parameters, values))
             {
-                using (SqlConnection sqlConnection = new SqlConnection(connectionString))
-                {
-                    sqlConnection.Open();
-                    SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
-                    sqlCommand.Parameters.AddWithValue("@packageID", PackageID);
-                    sqlCommand.Parameters.AddWithValue("@packageName", PackageName);
-                    sqlCommand.Parameters.AddWithValue("@maxKm", MaxKm);
-                    sqlCommand.Parameters.AddWithValue("@maxHour", MaxHour);
-                    sqlCommand.Parameters.AddWithValue("@extraKmRate", ExtraKmRate);
-                    sqlCommand.Parameters.AddWithValue("@extraHourRate", ExtraHourRate);
-                    sqlCommand.Parameters.AddWithValue("@standardRate", StandardRate);
-                    sqlCommand.ExecuteNonQuery();
-                    return true;
-                }
+                MessageBox.Show("Package type details successfully registered", "Registration successful", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return new PackageType(packageID, packageName, maxKm, maxHour, extraKmRate, extraHourRate, standardRate);
             }
-            catch (Exception e)
-            {
-                MessageBox.Show($"{e}", "An error occurred when regstering package type", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-            return false;
+            MessageBox.Show("Failed to register package type information", "Registration failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            return null;
         }
 
-        public bool UpdatePackageType(int packageID, string packageName, int maxKm, int maxHour, double extraKmRate, double extraHourRate, double standardRate)
+        public void UpdatePackageType(string packageName, int maxKm, int maxHour, double extraKmRate, double extraHourRate, double standardRate)
         {
             string query = "UPDATE PackageType SET packageName = @packageName, maxKm = @maxKm, maxHour = @maxHour, extraKmRate = @extraKmRate, " +
                 "extraHourRate = @extraHourRate, standardRate = @standardRate WHERE packageID = @packageID";
+            string[] parameters = { "@packageName", "@maxKm", "@maxHour", "@extraKmRate", " @extraHourRate", "@standardRate", "PackageID"};
+            object[] values = { packageName, maxKm, maxHour, extraKmRate, extraHourRate, standardRate, PackageID };
 
-            try
+            if (queryHandler.HandleInsertDeleteUpdateQuery(query, parameters, values))
             {
-                using (SqlConnection sqlConnection = new SqlConnection(connectionString))
-                {
-                    sqlConnection.Open();
-                    SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
-                    sqlCommand.Parameters.AddWithValue("@packageID", packageID);
-                    sqlCommand.Parameters.AddWithValue("@packageName", packageName);
-                    sqlCommand.Parameters.AddWithValue("@maxKm", maxKm);
-                    sqlCommand.Parameters.AddWithValue("@maxHour", maxHour);
-                    sqlCommand.Parameters.AddWithValue("@extraKmRate", extraKmRate);
-                    sqlCommand.Parameters.AddWithValue("@extraHourRate", extraHourRate);
-                    sqlCommand.Parameters.AddWithValue("@standardRate", standardRate);
-                    sqlCommand.ExecuteNonQuery();
-                    return true;
-                }
+                MessageBox.Show("Package type details successfully updated", "Update successful", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
             }
-            catch (Exception e)
-            {
-                MessageBox.Show($"{e}", "An error occurred when updating", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-            return false;
+            MessageBox.Show("Failed to update package type information", "Update failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
 
-        public bool DeletePackageType(int packageID)
+        public void DeletePackageType()
         {
             string query = "DELETE FROM PackageType WHERE packageID = @packageID";
 
-            try
+            string[] parameters = { "PackageID" };
+            object[] values = { PackageID };
+
+            if (queryHandler.HandleInsertDeleteUpdateQuery(query, parameters, values))
             {
-                using (SqlConnection sqlConnection = new SqlConnection(connectionString))
-                {
-                    sqlConnection.Open();
-                    SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
-                    sqlCommand.Parameters.AddWithValue("@packageID", packageID);
-                    sqlCommand.ExecuteNonQuery();
-                    return true;
-                }
+                MessageBox.Show("Package type details successfully deleted", "Delete successful", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
             }
-            catch (Exception e)
-            {
-                MessageBox.Show($"{e}", "An error occurred when deleting", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-            return false;
+            MessageBox.Show("Failed to delete package type information", "Delete failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
     }
 }
