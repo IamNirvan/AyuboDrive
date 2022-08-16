c41c5ea2d4bf393e896cfbad8d773427afc3b123
Shalin Kulawardane <nirvan.work@gmail.com>
2022-08-16 18:05:24 +0530

    Modify User.cs

--- AyuboDrive/User.cs
+++ AyuboDrive/User.cs
@@ -8,16 +8,16 @@ namespace AyuboDrive
 {
     public class User
     {
-        public string _userAccountID;
-        public string _firstName;
-        public string _lastName;
+        public string UserAccountID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         private static readonly QueryHandler queryHandler = new QueryHandler();
 
         private User(string userAccountID, string firstName, string lastName, string userName)
         {
-            _userAccountID = userAccountID;
-            _firstName = firstName;
-            _lastName = lastName;
+            UserAccountID = userAccountID;
+            FirstName = firstName;
+            LastName = lastName;
         }
 
         // Invoke this method first to login. If the login is successful, a user object with the appropriate data will be returned.
@@ -27,8 +27,8 @@ namespace AyuboDrive
             string[] parameters = { "@userName", "@password"};
             object[] values = {userName, password};
 
-            // Invoke the select query handler here
-            if(true)
+            // Check if the user account exists in the database             
+            if(queryHandler.SelectQueryHandler(query).Rows.Count == 1)
             {
                 return new User("userAccountID", "FirstName", "LastName", "UserName");
             }
@@ -46,15 +46,28 @@ namespace AyuboDrive
             string[] parameters = { "@firstName", "@lastName", "@userName", "@password" };
             object[] values = { firstName, lastName, userName, password};
 
-            //return queryHandler.HandleInsertDeleteUpdateQuery(query, parameters, values);
-            return false;
+            return queryHandler.HandleInsertDeleteUpdateQuery(query, parameters, values);
         }
 
         public bool DeleteAccount()
         {
             string query = "DELETE FROM User WHERE userAccountID = @userAccountID";
             string[] parameters = { "@userAccountID" };
-            object[] values = { _userAccountID };
+            object[] values = { UserAccountID };
+
+            return queryHandler.HandleInsertDeleteUpdateQuery(query, parameters, values);
+        }
+
+        public bool UpdateAccount(string firstName, string lastName, string userName, string password)
+        {
+            string query = "UPDATE User SET firstName = @firstName, lastName = @lastName, userName = @userName, " +
+                "password = @password WHERE userAccountID = @userAccountID";
+            string[] parameters = { "@firstName", "@lastName", "@userName", "@password", "@userAccountID" };
+            object[] values = { firstName, lastName, userName, password, UserAccountID };
+
+            // Modify the attributes to reflect the changes in the existing object
+            FirstName = firstName;
+            LastName = lastName;
 
             return queryHandler.HandleInsertDeleteUpdateQuery(query, parameters, values);
         }
