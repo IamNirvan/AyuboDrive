b2447726d55999a52a060f978784fd41bcfebb3d
Shalin Kulawardane <nirvan.work@gmail.com>
2022-09-04 18:31:16 +0530

    Added the interface for data manipulation

---
+++ AyuboDrive/Interfaces/IDatabaseManipulator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AyuboDrive.Interfaces
+{
+    public interface IDatabaseManipulator
+    {
+        bool Insert();
+
+        bool Delete(string ID);
+
+        bool Update(string ID);
+    }
+}
