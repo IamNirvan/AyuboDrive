76a59cdfc76c811bb7f4599fdbbf52cbc1abcb09
Shalin Kulawardane <nirvan.work@gmail.com>
2022-09-12 17:42:31 +0530

    Modified User to reflect changes in the database

--- AyuboDrive/User.cs
+++ AyuboDrive/User.cs
@@ -1,4 +1,5 @@
-﻿using AyuboDrive.Utility;
+﻿using AyuboDrive.Enums;
+using AyuboDrive.Utility;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -10,18 +11,43 @@ namespace AyuboDrive
 {
     public class User
     {
-        public string _userName;
-        private string _password;
+        public string UserName;
+        private readonly string _password;
         public string FirstName;
         public string LastName;
+        private GenderOptions _gender;
+        private readonly string _imagePath; // Image for the profile picture
         private static readonly QueryHandler s_queryHandler = new QueryHandler();
 
-        public User(string userName, string password, string firstName, string lastName)
+        public User(string userName, string password, string firstName, string lastName, GenderOptions gender, string imagePath)
         {
-            _userName = userName;
+            UserName = userName;
             _password = password;
             FirstName = firstName;
             LastName = lastName;
+            _gender = gender;
+            _imagePath = imagePath;
+        }
+        /// <summary>
+        /// Converts the string version of the gender into the 
+        ///  GenderOptions version
+        /// </summary>
+        /// <param name="gender">The gender in string format</param>
+        /// <returns>The gender in GenderOptions format</returns>
+        private static GenderOptions GetGender(string gender)
+        {
+            if (gender.Equals("male"))
+            {
+                return GenderOptions.MALE;
+            }
+            else if (gender.Equals("female"))
+            {
+                return GenderOptions.FEMALE;
+            }
+            else
+            {
+                return GenderOptions.OTHER;
+            }
         }
 
         public static User Login(string userName, string password)
@@ -35,23 +61,20 @@ namespace AyuboDrive
                     dataTable.Rows[0][1].ToString(),
                     dataTable.Rows[0][2].ToString(),
                     dataTable.Rows[0][3].ToString(),
-                    dataTable.Rows[0][4].ToString()
+                    dataTable.Rows[0][4].ToString(),
+                    GetGender(dataTable.Rows[0][5].ToString()),
+                    dataTable.Rows[0][6].ToString()
                     );
                 return user;
             }
             return null;
         }
-
-        public bool Logout()
-        {
-            return false;
-        }
-
+        
         public bool Insert()
         {
-            string query = "INSERT INTO UserAccount VALUES (@userName, @password, @firstName, @lastName)";
-            string[] parameters = { "@userName", "@password", "@firstName", "@lastName" };
-            object[] values = { _userName, _password, FirstName, LastName };
+            string query = "INSERT INTO UserAccount VALUES (@userName, @password, @firstName, @lastName, @gender, @imagePath)";
+            string[] parameters = { "@userName", "@password", "@firstName", "@lastName", "@gender", "@imagePath" };
+            object[] values = { UserName, _password, FirstName, LastName, _gender, _imagePath };
 
             if (s_queryHandler.InsertQueryHandler(query, parameters, values))
             {
@@ -62,7 +85,7 @@ namespace AyuboDrive
             return false;
         }
 
-        public bool Delete(string ID)
+        public static bool Delete(string ID)
         {
             string query = "DELETE FROM UserAccount WHERE userAccountID = @userAccountID";
             string[] parameters = { "@userAccountID" };
@@ -80,9 +103,11 @@ namespace AyuboDrive
         public bool Update(string ID)
         {
             string query = "UPDATE UserAccount SET userName = @userName, password = @password, " +
-                "firstName = @firstName, lastName = @lastName WHERE userAccountID = @userAccountID";
-            string[] parameters = { "@userName", "@password", "@firstName", "@lastName", "@userAccountID" };
-            object[] values = { _userName, _password, FirstName, LastName, ID };
+                "firstName = @firstName, lastName = @lastName, gender = @gender, imagePath = @imagePath " +
+                "WHERE userAccountID = @userAccountID";
+            string[] parameters = { "@userName", "@password", "@firstName", "@lastName", "@imagePath",
+                "@gender", "@userAccountID"};
+            object[] values = { UserName, _password, FirstName, LastName, _gender, _imagePath, ID };
 
             if (s_queryHandler.UpdateQueryHandler(query, parameters, values))
             {
