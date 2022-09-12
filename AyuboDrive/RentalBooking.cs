f30467e147e20afebc79c05dbc19888af23645b2
Shalin Kulawardane <nirvan.work@gmail.com>
2022-09-12 18:04:26 +0530

    Applied a naming convention and made small optimizations

--- AyuboDrive/RentalBooking.cs
+++ AyuboDrive/RentalBooking.cs
@@ -12,14 +12,18 @@ namespace AyuboDrive
     {
         private readonly string _vehicleTypeID;
         private readonly string _vehicleID;
-        private readonly string _driverID;
+        private string _driverID;
         private readonly string _customerID;
         private readonly string _startDate;
         private readonly string _endDate;
+        private readonly string _rentalStatus;
+        private readonly string _paymentStatus;
         private static readonly QueryHandler s_queryHandler = new QueryHandler();
+        public static readonly string NullValuePlaceHolder = "No driver";
+        private object[] _values;
 
         public RentalBooking(string vehicleTypeID, string vehicleID, string driverID, string customerID, 
-            string startDate, string endDate)
+            string startDate, string endDate, string rentalStatus, string paymentStatus)
         {
             _vehicleTypeID = vehicleTypeID;
             _vehicleID = vehicleID;
@@ -27,30 +31,26 @@ namespace AyuboDrive
             _customerID = customerID;
             _startDate = startDate;
             _endDate = endDate;
+            _rentalStatus = rentalStatus;
+            _paymentStatus = paymentStatus;
+
+            SetValues();
+        }
+
+        private void SetValues()
+        {
+            _driverID = _driverID == null ? NullValuePlaceHolder : _driverID;
+            _values = new object[] { _vehicleTypeID, _vehicleID, _driverID, _customerID, _startDate, _endDate, _rentalStatus, _paymentStatus };
         }
 
         public bool Insert()
         {
-            string query;
-            string[] parameters;
-            object[] values;
+            string query = "INSERT INTO rentalBooking VALUES(@vehicleTypeID, @vehicleID, @driverID, " +
+                "@customerID, @startDate, @endDate, @rentalStatus, @paymentStatus)";
+            string[] parameters = { "@vehicleTypeID", "@vehicleID", "@driverID", "@customerID",
+                    "@startDate", "@endDate", "@rentalStatus", "@paymentStatus" };
 
-            if (_driverID == null)
-            {
-                query = "INSERT INTO rentalBooking(vehicleTypeID, vehicleID, customerID, startDate, endDate) " +
-                    "VALUES(@vehicleTypeID, @vehicleID, @customerID, @startDate, @endDate)";
-                parameters = new string[]{ "@vehicleTypeID", "@vehicleID", "@customerID", "@startDate", "@endDate" };
-                values = new object[]{ _vehicleTypeID, _vehicleID, _customerID, _startDate, _endDate};
-            }
-            else
-            {
-                query = "INSERT INTO rentalBooking VALUES(@vehicleTypeID, @vehicleID, @driverID, " +
-                "@customerID, @startDate, @endDate)";
-                parameters = new string[]{ "@vehicleTypeID", "@vehicleID", "@driverID", "@customerID",
-                    "@startDate", "@endDate" };
-                values = new object[]{ _vehicleTypeID, _vehicleID, _driverID, _customerID, _startDate, _endDate};
-            }
-            if (s_queryHandler.InsertQueryHandler(query, parameters, values))
+            if (s_queryHandler.InsertQueryHandler(query, parameters, _values))
             {
                 MessagePrinter.PrintToConsole("Rental booking details successfully inserted", "Operation successful");
                 return true;
@@ -59,7 +59,7 @@ namespace AyuboDrive
             return false;
         }
 
-        public bool Delete(string ID)
+        public static bool Delete(string ID)
         {
             string query = "DELETE FROM rentalBooking WHERE bookingID = @bookingID";
             string[] parameters = { "@bookingID" };
@@ -77,13 +77,12 @@ namespace AyuboDrive
         public bool Update(string ID)
         {
             string query = "UPDATE rentalBooking SET vehicleTypeID = @vehicleTypeID, vehicleID = @vehicleID, " +
-                "driverID = @driverID, customerID = @customerID, startDate = @startDate, endDate = @endDate " +
-                "WHERE bookingID = @bookingID";
-            string[] parameters = { "@vehicleTypeID", "@vehicleID", "@driverID",
-                "@customerID", "@startDate", "@endDate", "@bookingID" };
-            object[] values = { _vehicleTypeID, _vehicleID, _driverID, _customerID, _startDate, _endDate, ID };
-
-            if (s_queryHandler.InsertQueryHandler(query, parameters, values))
+                "driverID = @driverID, customerID = @customerID, startDate = @startDate, endDate = @endDate, " +
+                "rentalStatus = @rentalStatus, paymentStatus = @paymentStatus WHERE bookingID = @bookingID";
+            string[] parameters = { "@vehicleTypeID", "@vehicleID", "@driverID", "@customerID",
+                "@startDate", "@endDate", "@rentalStatus", "@paymentStatus", "@bookingID" };
+            
+            if (s_queryHandler.InsertQueryHandler(query, parameters, _values))
             {
                 MessagePrinter.PrintToConsole("Rental booking details successfully updated", "Operation successful");
                 return true;
