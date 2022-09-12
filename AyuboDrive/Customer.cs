6408966c1ae3505c445a492cf4a043559df61394
Shalin Kulawardane <nirvan.work@gmail.com>
2022-09-12 17:47:57 +0530

    Modified customer to match the changes in the database

--- AyuboDrive/Customer.cs
+++ AyuboDrive/Customer.cs
@@ -1,4 +1,5 @@
-﻿using AyuboDrive.Interfaces;
+﻿using AyuboDrive.Enums;
+using AyuboDrive.Interfaces;
 using AyuboDrive.Utility;
 using System;
 using System.Collections.Generic;
@@ -10,33 +11,32 @@ using System.Windows.Forms;
 
 namespace AyuboDrive
 {
-    /// <summary>
-    /// Represents a customer. It houses methods that can be used to insert, delete 
-    /// and update customer information in the customer table.
-    /// </summary>
+
     class Customer : IDatabaseManipulator
     {
-        private string _customerNIC { get; set; }
-        private string _firstName { get; set; }
-        private string _lastName { get; set; }
-        private string _contactNumber { get; set; }
-        private static QueryHandler _queryHandler = new QueryHandler();
+        private readonly string _customerNIC;
+        private readonly string _firstName;
+        private readonly string _lastName;
+        private readonly string _contactNumber;
+        private readonly GenderOptions _gender;
+        private readonly static QueryHandler s_queryHandler = new QueryHandler();
 
-        public Customer(string customerNIC, string firstName, string lastName, string contactNumber)
+        public Customer(string customerNIC, string firstName, string lastName, string contactNumber, GenderOptions gender)
         {
             _customerNIC = customerNIC;
             _firstName = firstName;
             _lastName = lastName;
             _contactNumber = contactNumber;
+            _gender = gender;
         }
 
         public bool Insert()
         {
-            string query = "INSERT INTO Customer VALUES(@customerNIC, @firstName, @lastName, @contactNumber)";
-            string[] parameters = { "@customerNIC", "@firstName", "@lastName", "@contactNumber" };
-            object[] values = { _customerNIC, _firstName, _lastName, _contactNumber };
+            string query = "INSERT INTO Customer VALUES(@customerNIC, @firstName, @lastName, @contactNumber, @gender)";
+            string[] parameters = { "@customerNIC", "@firstName", "@lastName", "@contactNumber", "@gender" };
+            object[] values = { _customerNIC, _firstName, _lastName, _contactNumber, _gender };
 
-            if (_queryHandler.InsertQueryHandler(query, parameters, values))
+            if (s_queryHandler.InsertQueryHandler(query, parameters, values))
             {
                 MessagePrinter.PrintToConsole("Customer details successfully inserted", "Operation successful");
                 return true;
@@ -51,7 +51,7 @@ namespace AyuboDrive
             string[] parameters = { "@customerID" };
             object[] values = { ID };
 
-            if (_queryHandler.DeleteQueryHandler(query, parameters, values))
+            if (s_queryHandler.DeleteQueryHandler(query, parameters, values))
             {
                 MessagePrinter.PrintToConsole("Customer details successfully deleted", "Operation successful");
                 return true;
@@ -63,11 +63,11 @@ namespace AyuboDrive
         public bool Update(string ID)
         {
             string query = "UPDATE Customer SET customerNIC = @customerNIC, firstName = @firstName, " +
-                "lastName = @lastName, contactNumber = @contactNumber WHERE customerID = @customerID";
-            string[] parameters = { "@customerNIC", "@firstName", "@lastName", "@contactNumber", "@customerID" };
+                "lastName = @lastName, contactNumber = @contactNumber, gender = @gender WHERE customerID = @customerID";
+            string[] parameters = { "@customerNIC", "@firstName", "@lastName", "@contactNumber", "@gender", "@customerID" };
             object[] values = { _customerNIC, _firstName, _lastName, _contactNumber, ID };
 
-            if (_queryHandler.UpdateQueryHandler(query, parameters, values))
+            if (s_queryHandler.UpdateQueryHandler(query, parameters, values))
             {
                 MessagePrinter.PrintToConsole("Customer details successfully updated", "Operation successful");
                 return true;
