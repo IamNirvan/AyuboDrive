ca8f5408cafc73559d71219389d9d7eac622688c
Shalin Kulawardane <nirvan.work@gmail.com>
2022-09-12 17:43:55 +0530

    Modified User to reflect changes in the database

--- AyuboDrive/User.cs
+++ AyuboDrive/User.cs
@@ -1,5 +1,4 @@
-﻿using AyuboDrive.Enums;
-using AyuboDrive.Utility;
+﻿using AyuboDrive.Utility;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -15,39 +14,16 @@ namespace AyuboDrive
         private readonly string _password;
         public string FirstName;
         public string LastName;
-        private GenderOptions _gender;
         private readonly string _imagePath; // Image for the profile picture
         private static readonly QueryHandler s_queryHandler = new QueryHandler();
 
-        public User(string userName, string password, string firstName, string lastName, GenderOptions gender, string imagePath)
+        public User(string userName, string password, string firstName, string lastName, string imagePath)
         {
             UserName = userName;
             _password = password;
             FirstName = firstName;
             LastName = lastName;
-            _gender = gender;
             _imagePath = imagePath;
-        }
-        /// <summary>
-        /// Converts the string version of the gender into the 
-        ///  GenderOptions version
-        /// </summary>
-        /// <param name="gender">The gender in string format</param>
-        /// <returns>The gender in GenderOptions format</returns>
-        private static GenderOptions GetGender(string gender)
-        {
-            if (gender.Equals("male"))
-            {
-                return GenderOptions.MALE;
-            }
-            else if (gender.Equals("female"))
-            {
-                return GenderOptions.FEMALE;
-            }
-            else
-            {
-                return GenderOptions.OTHER;
-            }
         }
 
         public static User Login(string userName, string password)
@@ -62,8 +38,7 @@ namespace AyuboDrive
                     dataTable.Rows[0][2].ToString(),
                     dataTable.Rows[0][3].ToString(),
                     dataTable.Rows[0][4].ToString(),
-                    GetGender(dataTable.Rows[0][5].ToString()),
-                    dataTable.Rows[0][6].ToString()
+                    dataTable.Rows[0][5].ToString()
                     );
                 return user;
             }
@@ -72,9 +47,9 @@ namespace AyuboDrive
         
         public bool Insert()
         {
-            string query = "INSERT INTO UserAccount VALUES (@userName, @password, @firstName, @lastName, @gender, @imagePath)";
-            string[] parameters = { "@userName", "@password", "@firstName", "@lastName", "@gender", "@imagePath" };
-            object[] values = { UserName, _password, FirstName, LastName, _gender, _imagePath };
+            string query = "INSERT INTO UserAccount VALUES (@userName, @password, @firstName, @lastName, @imagePath)";
+            string[] parameters = { "@userName", "@password", "@firstName", "@lastName", "@imagePath" };
+            object[] values = { UserName, _password, FirstName, LastName, _imagePath };
 
             if (s_queryHandler.InsertQueryHandler(query, parameters, values))
             {
@@ -103,11 +78,10 @@ namespace AyuboDrive
         public bool Update(string ID)
         {
             string query = "UPDATE UserAccount SET userName = @userName, password = @password, " +
-                "firstName = @firstName, lastName = @lastName, gender = @gender, imagePath = @imagePath " +
+                "firstName = @firstName, lastName = @lastName, imagePath = @imagePath " +
                 "WHERE userAccountID = @userAccountID";
-            string[] parameters = { "@userName", "@password", "@firstName", "@lastName", "@imagePath",
-                "@gender", "@userAccountID"};
-            object[] values = { UserName, _password, FirstName, LastName, _gender, _imagePath, ID };
+            string[] parameters = { "@userName", "@password", "@firstName", "@lastName", "@imagePath", "@userAccountID"};
+            object[] values = { UserName, _password, FirstName, LastName, _imagePath, ID };
 
             if (s_queryHandler.UpdateQueryHandler(query, parameters, values))
             {
