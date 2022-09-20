440f9298cad52b1c1c65243eb66b788b2c6940e5
Shalin Kulawardane <nirvan.work@gmail.com>
2022-09-20 11:40:37 +0530

    Added DayWeekMonth calculation engine

---
+++ AyuboDrive/Engines/DayWeekMonthCalculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AyuboDrive.Utility
+{
+    static class DayWeekMonthCalculator
+    {
+        public static int[] GetDaysWeeksMonths(int days)
+        {
+            int months = 0;
+            int weeks = 0;
+
+            while(days >= 7)
+            {
+                days -= 7;
+                weeks++;
+
+                if(weeks == 4)
+                {
+                    months++;
+                    weeks = 0;
+                }
+            }
+            return new int[] {days, weeks, months};
+        }
+    }
+}
